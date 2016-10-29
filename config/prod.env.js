d1973c0f82e40ca01db378908d385af1f4a4a83e
Walter Anderson <walter@kimbia.com>
2016-10-29 15:02:36 -0500

    moving API endpoints into env config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,6 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_BASE_URL: '""',
+  API_AUTH_ENDPOINT: '"/auth/local"',
+  API_USER_ENDPOINT: '"/users/me"',
 }
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,11 @@ import { requestInterceptor } from './interceptors';
 
 http.interceptors.request.use(requestInterceptor);
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const {
+  API_BASE_URL,
+  API_AUTH_ENDPOINT,
+  API_USER_ENDPOINT,
+} = process.env;
 
 // const DEFAULT_HEADERS = {
 //   'Content-Type': 'application/json',
@@ -11,12 +15,12 @@ const API_BASE_URL = 'http://localhost:8000/api';
 
 export default {
   authenticate(creds) {
-    const url = `${API_BASE_URL}/auth/local`;
+    const url = API_BASE_URL + API_AUTH_ENDPOINT;
     return http.post(url, creds);
   },
 
   getUser() {
-    const url = `${API_BASE_URL}/users/me`;
+    const url = API_BASE_URL + API_USER_ENDPOINT;
     return http.get(url);
   },
 };
